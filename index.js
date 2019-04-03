2980e5bec67ad25b5f78dd9fde0561a616be3145
MJmajiong <936233505@qq.com>
2019-04-04 02:41:16 +0800

    提提交

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var globalConfig = require("./config");
+var loader = require("./loader");
+var cookie = require("cookie-parser");                     //cookie的中间件
+var multer = require("multer");                          //解析文件上传的中间件
+
+var app = new express();
+var uploadSingle = multer({dest:"./file/"});
+
+app.use(express.static(globalConfig["page_path"]));
+app.use(cookie());
+
+
+app.get("/api/*", function (request, response, next){
+    console.log(request.cookies);
+    // if(request.cookies.id){
+    //     next()
+    // }else {
+    //     response.redirect("/login.html");
+    // }
+    next()
+});
+
+
+app.get("/api/getAllStudent", loader.get("/api/getAllStudent"));
+app.get("/api/addStudent", loader.get("/api/addStudent"));
+app.get("/login", loader.get("/login"));
+app.post("/upload", uploadSingle.single("filename"), loader.get("/upload"));      //指定哪个字段是文件
+app.get('/getPic', loader.get("/getPic"));
+
+app.listen(globalConfig["port"]);
